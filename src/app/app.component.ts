efe48a8125681516fa034d21729413a53d76db04
Rideek <rabinskid@rabdev.pl>
2024-06-20 10:12:05 +0200

    gA test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { AboutComponent } from './component/about/about.component';
 import { VideoSecitonComponent } from './component/video-seciton/video-seciton.component';
 import { YouHaveSectionComponent } from './component/you-have-section/you-have-section.component';
 import { CompozitionSectionComponent } from './component/compozition-section/compozition-section.component';
-import { filter } from 'rxjs';
-import { AnalyticsService } from './analytics.service';
 
 @Component({
   selector: 'app-root',
@@ -22,21 +20,4 @@ import { AnalyticsService } from './analytics.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
-export class AppComponent {
-  constructor(
-    private router: Router,
-    private analyticsService: AnalyticsService
-  ) {}
-
-  ngOnInit() {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        this.analyticsService.sendEvent(
-          'page_view',
-          'Navigation',
-          event.urlAfterRedirects
-        );
-      });
-  }
-}
+export class AppComponent {}
